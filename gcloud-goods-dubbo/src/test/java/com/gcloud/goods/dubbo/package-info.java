bb02d078b30fd02b94fb3daa08436de3829748d3
janenone2163.com <janenone@163.com>
2016-06-13 18:16:28 +0800

    add:dubbo

--- gcloud-goods-dubbo/src/test/java/com/gcloud/goods/dubbo/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Title: package-info
- * @Package com.gcloud.goods.dubbo
- * @Description: ${TODO}(用一句话描述该文件做什么)
- * @author ChenJin
- * @date 2016/6/12 17:16
- * @version V1.0
- */
-package com.gcloud.goods.dubbo;
