d5f464f3269ff7f43a46d8f75340068f6f980fb9
janenone2163.com <janenone@163.com>
2016-06-14 13:44:22 +0800

    mod: spring-dubbo.xml

--- gcloud-goods-web/src/main/java/com/gcloud/goods/web/event/AreaListEvent.java
+++ gcloud-goods-web/src/main/java/com/gcloud/goods/web/event/AreaListEvent.java
@@ -4,15 +4,9 @@ import com.gcloud.event.core.CommonEventSource;
 import com.gcloud.event.core.EventSourceBase;
 import com.gcloud.event.core.IEventListener;
 import com.gcloud.event.core.annotation.EventBind;
-import com.gcloud.trade.core.IAreaService;
-import com.gcloud.trade.domain.Area;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * @author ChenJin
@@ -28,15 +22,8 @@ public class AreaListEvent implements IEventListener {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
-    @Autowired
-    IAreaService areaService;
-
     @Override
     public void onObserved(EventSourceBase source) {
-
-        List<Area> areaList =areaService.queryAreaList(new HashMap<String, Object>());
-        logger.info(areaList.size());
-
         CommonEventSource evt = (CommonEventSource) source;
         String userName = evt.getArg(0, String.class);
         logger.info("您好! " + userName);
